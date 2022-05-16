504cc8412e15945cf5b212b0771ced89f253f33f
hope <proftoby4real@hotmail.com>
2022-05-16 16:02:21 +0100

    fixed some bugs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,29 @@
 import type {  NextPage } from 'next'
+import { useRouter } from 'next/router';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import useAuth from '../Hooks/useAuth'
 
-const Home: NextPage = () => {
-  return (
+const Home = () => {
+   const router = useRouter();
+   React.useEffect(() => {
+   const idToken = sessionStorage.getItem('idToken');
+   const isLoggedin = !!idToken;
+   setInterval(() => {
+    if(!isLoggedin){
+       router.push('/Login')
+     } else {
+      router.push('/users')
+     }
+   }, 100)
+   return (() => clearInterval())
+   }, [])  
+   return (
     <>
-    <h2>Nothing here</h2>
+    
     </>
-
-  )
+   )
 
 }
 
-  export  async function getStaticProps(){
-     return {
-       redirect: {
-         destination: '/Login'
-       },
-       props: {
-         some: 'nothing'
-       }
-     }
-  }
 export default Home

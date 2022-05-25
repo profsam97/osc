0ae916a9058004f66263120a579ca15a1b616dd4
hope <proftoby4real@hotmail.com>
2022-05-25 09:09:49 +0100

    added a new ui/ux

--- Components/Users/Dashboard.tsx
+++ Components/Users/Dashboard.tsx
@@ -58,8 +58,8 @@ const Dashboard:NextPage = () => {
           return (
           <>
           {isLoading ? 
-          <Grid key={data.title} item xs={12} sm={6} md={4} >
-          <Stack spacing={0.5}  sx={{width: {xs: 220, sm: 200, md: 250}}}>
+          <Grid key={data.title} item xs={12} sm={6} md={4}>
+          <Stack spacing={0.5}  sx={{width: {xs: 220, sm: 200, md: 250, lg: 310, xl: 350}}}>
           <Skeleton variant='rectangular'  animation='wave' height={158}  /> 
           <Skeleton  animation='wave' />
           </Stack>
